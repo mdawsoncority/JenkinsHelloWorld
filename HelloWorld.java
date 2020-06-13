afda4f65d2825fe32af3662658785134b0bd1f10
mdawsoncority <mark.dawson@cority.com>
2020-06-13 16:01:17 -0400

    Update pring

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
 	public static void main(String[] args){
-		System.out.println("Hello World and welcome from remote git!!!!");
+		System.out.println("Hello World and welcome from remote git!");
 	}
 }
